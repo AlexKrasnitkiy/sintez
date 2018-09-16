c89e4e334eb9c48130d123e9b9a1e89ca5b240c4
Alex <alexkrasnitskiy@gmail.com>
2018-09-16 22:23:16 +0300

    nextPart

--- src/angular/core/core.controller.js
+++ src/angular/core/core.controller.js
@@ -12,22 +12,22 @@
         var vm = this;
         vm.categories = {};
         vm.difficulty = [
-        {
-            name: 'Easy'
-        },
-        {
-            name: 'Medium'
-        },
-        {
-            name: 'Hard'
-        }];
+            {
+                name: 'Easy'
+            },
+            {
+                name: 'Medium'
+            },
+            {
+                name: 'Hard'
+            }];
         vm.total = {};
 
-        vm.generate = generate;
+        vm.generateQuestions = generateQuestions;
 
-		
+
         activate();
-        
+
         function activate() {
             getAllCategories();
         }
@@ -40,41 +40,58 @@
             })
         }
 
-        function generate() {
+        function generateQuestions() {
             var getAmount = document.getElementById('amount');
             vm.amount = parseInt(getAmount.value);
-            if (vm.amount !== null && vm.amount !== undefined){
+            if (vm.amount !== null && vm.amount !== undefined) {
                 vm.total.amount = vm.amount;
             }
-            if (vm.selectedCategory !== null && vm.selectedCategory !== undefined){
+            if (vm.selectedCategory !== null && vm.selectedCategory !== undefined) {
                 vm.total.category = vm.selectedCategory;
             }
-            if (vm.selectedDifficulty !== null && vm.selectedDifficulty !== undefined &&  vm.selectedDifficulty.name !== null && vm.selectedDifficulty.name !== undefined){
+            if (vm.selectedDifficulty !== null && vm.selectedDifficulty !== undefined && vm.selectedDifficulty.name !== null && vm.selectedDifficulty.name !== undefined) {
                 console.log('DO NOT WORK');
                 vm.total.difficulty = vm.selectedDifficulty.name.toLowerCase();
             }
-            if (vm.total !== null && vm.total !== undefined){
-               if (vm.amount === null || vm.amount === undefined || isNaN(vm.amount)){
-                   alert('Amount cannot be empty')
-               } else {
-                   CoreService.getQuestions(vm.total).then(function (response) {
-                       //тут надо что то придумать, очищает фронт но на бекенд идет хрень
-                       vm.selectedDifficulty  = {};
-                       vm.selectedCategory = {};
-                       vm.amount = 10;
-                       vm.questions = response.results;
-                   }).catch(function (error) {
-                       console.log('error in generate', error)
-                   })
-               }
+            if (vm.total !== null && vm.total !== undefined) {
+                if (vm.amount === null || vm.amount === undefined || isNaN(vm.amount)) {
+                    alert('Amount cannot be empty')
+                } else {
+                    getQuestionsByFilter();
+                }
             }
 
+            function getQuestionsByFilter() {
+                CoreService.getQuestions(vm.total).then(function (response) {
+                    //тут надо что то придумать, очищает фронт но на бекенд идет хрень
+                    vm.selectedDifficulty = {};
+                    vm.selectedCategory = {};
+                    vm.amount = 10;
+                    //
+                    vm.questions = response.results;
+                    _.forEach(vm.questions, function (q) {
+                        q.incorrect_answers.push(q.correct_answer);
+                        q.incorrect_answers.shuffle();
+                    });
 
+
+                }).catch(function (error) {
+                    console.log('error in generate', error)
+                })
+            }
+
+            Array.prototype.shuffle = function (b) {
+                var i = this.length, j, t;
+                while (i) {
+                    j = Math.floor(( i-- ) * Math.random());
+                    t = b && typeof this[i].shuffle !== 'undefined' ? this[i].shuffle() : this[i];
+                    this[i] = this[j];
+                    this[j] = t;
+                }
+                return this;
+            };
 
         }
-
-
-
-        }
+    }
 
 })();
